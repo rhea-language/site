a3a75ea42e57647e49d3246dbee617050d7c03d0
Nathanne Isip <nathanneisip@gmail.com>
2024-12-15 17:57:31 +0800

    Component footer file template.

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const currentYear: number = new Date().getFullYear();
+</script>
+
+<template>
+    <footer class="mt-2 mb-4">
+        <hr class="mb-2" />
+        <p class="block w-full text-center">
+            Copyright 2024&mdash;{{ currentYear }} &copy;
+            <a href="https://github.com/nthnn" target="_blank">Nathanne Isip</a>
+        </p>
+    </footer>
+</template>
