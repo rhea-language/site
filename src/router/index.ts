c4134dcfbb0d6ed4b5e396827021fc50a3f132d1
Nathanne Isip <nathanneisip@gmail.com>
2024-12-15 18:00:18 +0800

    Reconfigured router file for Vite.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomeView from "@/views/HomeView.vue";
+import Playground from "@/views/Playground.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,9 +12,9 @@ const router = createRouter({
             component: HomeView,
         },
         {
-            path: '/about',
-            name: 'about',
-            component: () => import('../views/AboutView.vue'),
+            path: '/playground',
+            name: 'playground',
+            component: Playground
         },
     ],
 });
