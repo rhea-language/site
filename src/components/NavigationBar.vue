981983a86ddaeb7e23c85155c58eadcfb2f8a378
Nathanne Isip <nathanneisip@gmail.com>
2024-12-15 17:57:31 +0800

    Initial site navigation bar component.

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <nav class="navbar navbar-dark navbar-expand-lg shadow fixed-top m-0 py-2 w-100 bg-dark border-bottom border-gray">
+        <RouterLink class="navbar-brand pl-3" to="/">N8 <span class="desktop-only">Programming</span> Language</RouterLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-main" aria-controls="navbar-main" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse bg-gray" id="navbar-main">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav my-2 my-lg-0 pr-3">
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/playground">Playground</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/getting-started">Getting Started</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/download">Download</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/documentations">Documentations</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/references">References</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/n8lang/n8" target="_blank">GitHub</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
