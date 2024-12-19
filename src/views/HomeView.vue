038a912501401320161037a457e1a621d3db060c
Nathanne Isip <nathanneisip@gmail.com>
2024-12-20 01:31:00 +0800

    Improvements on layout and contents of the home page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
 import Sandbox from "@/components/Sandbox.vue";
+import {
+    BIconController,
+    BIconCpu,
+    BIconDownload,
+    BIconMotherboard,
+    BIconNewspaper,
+    BIconPcDisplay
+} from "bootstrap-icons-vue";
 </script>
 
 <template>
+    <br/>
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
@@ -26,11 +35,11 @@ import Sandbox from "@/components/Sandbox.vue";
 
                 <div class="row gx-0 gy-0 mt-1 px-2">
                     <div class="col-6 px-1">
-                        <RouterLink class="btn btn-primary w-100" to="/documentations">Learn More</RouterLink>
+                        <RouterLink class="btn btn-primary w-100" to="/documentations"><BIconNewspaper /><span class="pl-2">Learn More</span></RouterLink>
                     </div>
 
                     <div class="col-6 px-1">
-                        <RouterLink class="btn btn-secondary w-100" to="/download">Download</RouterLink>
+                        <RouterLink class="btn btn-secondary w-100" to="/download"><BIconDownload /><span class="pl-2">Download</span></RouterLink>
                     </div>
                 </div>
                 <br/>
@@ -46,60 +55,58 @@ import Sandbox from "@/components/Sandbox.vue";
 
     <div class="bg-dark py-4 shadow">
         <div class="container">
-            <center>
+            <div align="center">
                 <h2>Use Cases</h2>
                 <p>N8 is a versatile, general-purpose dynamic programming language designed to support a wide range of software development applications.</p>
-            </center>
-
-            <div class="row equal-cols">
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="card-title pt-2">High-performance Scientific Computing</h4>
-                        </div>
-                        <div class="card-body">
-                            <p>N8's ability to perform direct mathematical operations on vectors and arrays makes it ideal for scientific computing applications such as simulations, data analysis, and mathematical modeling. Its concurrency support ensures that heavy computational tasks can run efficiently on multi-core CPUs.</p>
-                        </div>
-                    </div>
-                    <br/>
-                </div>
-
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="card-title pt-2">Data Pipelines & Extract-Transform-Load</h4>
-                        </div>
-                        <div class="card-body">
-                            <p>With the N8's expressiveness, it can be used to build efficient data pipelines for ETL processes in big data applications. Its support for complex data structures and built-in concurrency enables scalable, high-throughput data processing, making it ideal for tasks like data aggregation, transformation, and loading in distributed systems.</p>
-                        </div>
-                    </div>
-                    <br/>
-                </div>
             </div>
 
             <div class="row equal-cols">
-                <div class="col-lg-6">
+                <div class="col-lg-3 px-lg-1">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title pt-2">Machine Learning Model Prototyping</h4>
+                            <h3><BIconCpu/></h3>
+                            <h5 class="card-title m-0 p-0">High-performance Scientific Computing</h5>
                         </div>
-                        <div class="card-body">
+                        <div class="card-body m-0">
+                            <p>N8's ability to perform direct mathematical operations on vectors and arrays makes it ideal for scientific computing applications such as simulations, data analysis, and mathematical modeling. Its concurrency support ensures that heavy computational tasks can run efficiently on multi-core CPUs.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 px-lg-1">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3><BIconPcDisplay /></h3>
+                            <h5 class="card-title m-0 p-0">Data Pipelines and Extract-Transform-Load</h5>
+                        </div>
+                        <div class="card-body m-0">
+                            <p>N8 can be used to build efficient data pipelines for ETL processes in big data applications. Its support for complex data structures and built-in concurrency enables scalable, high-throughput data processing, making it ideal for tasks like data aggregation, transformation, and loading.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 px-lg-1">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3><BIconMotherboard /></h3>
+                            <h5 class="card-title m-0 p-0">Machine Learning Model Training and Prototyping</h5>
+                        </div>
+                        <div class="card-body m-0">
                             <p>N8 is ideal for Machine Learning and AI due to its efficient mathematical operations, built-in concurrency, flexible syntax, seamless integration with native libraries, and a simplified project structure, enabling rapid development and optimization of advanced algorithms and applications.</p>
                         </div>
                     </div>
-                    <br/>
                 </div>
 
-                <div class="col-lg-6">
+                <div class="col-lg-3 px-lg-1">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title pt-2">Game Development</h4>
+                            <h3><BIconController /></h3>
+                            <h5 class="card-title m-0 p-0">Foundational Graphics and Game Development</h5>
                         </div>
-                        <div class="card-body">
-                            <p>Language's native support for OpenGL, concurrency, vector operations is perfect for game engines that require real-time physics simulations, AI processing, and rendering pipelines. Developers can also easily interface with native libraries for advanced graphical rendering or sound processing.</p>
+                        <div class="card-body m-0">
+                            <p>Language's native support for OpenGL, concurrency, vector operations is perfect for game engines that require real-time physics simulations, AI processing, and rendering pipelines. Developers can also easily wrap and interface native libraries for advanced graphical rendering or sound processing.</p>
                         </div>
                     </div>
-                    <br/>
                 </div>
             </div>
         </div>
