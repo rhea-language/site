fad62d87a7c41371d6bd83f0c6bb51077beb2ad6
Nathanne Isip <nathanneisip@gmail.com>
2024-12-20 01:38:27 +0800

    Removed link decoration in Footer component.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@ const currentYear: number = new Date().getFullYear();
         <hr class="mb-2" />
         <p class="block w-full text-center">
             Copyright 2024&mdash;{{ currentYear }} &copy;
-            <a href="https://github.com/nthnn" target="_blank">Nathanne Isip</a>
+            <a href="https://github.com/nthnn" target="_blank" class="text-decoration-none">Nathanne Isip</a>
         </p>
     </footer>
 </template>
