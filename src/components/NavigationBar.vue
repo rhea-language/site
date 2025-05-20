469fc3075b60eaffe563af0a69a472ceae0fb1f1
Nathanne Isip <nathanneisip@gmail.com>
2025-05-21 06:15:44 +0800

    Updated NavigationBar component

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -7,51 +7,51 @@ const isActive = (path: string) => route.path === path;
 </script>
 
 <template>
-    <nav class="navbar navbar-dark navbar-expand-lg shadow fixed-top m-0 py-2 w-100 bg-dark border-bottom border-gray">
-        <RouterLink class="navbar-brand pl-3" to="/">Rhea <span class="desktop-only">Programming</span> Language</RouterLink>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-main" aria-controls="navbar-main" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg fixed-top border-bottom border-gray">
+        <RouterLink class="navbar-brand ms-4" to="/">Rhea <span class="desktop-only">Programming </span>Language</RouterLink>
+        <button class="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-main" aria-controls="navbar-main" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse bg-gray" id="navbar-main">
-            <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav my-2 my-lg-0 pr-3">
-                <li class="nav-item">
+        <div class="collapse navbar-collapse" id="navbar-main">
+            <ul class="navbar-nav me-auto"></ul>
+            <ul class="navbar-nav me-4">
+                <li class="nav-item ms-2">
                     <RouterLink
                         :class="['nav-link', { active: isActive('/') }]"
                         to="/"
-                    ><h5 class="d-inline"><BIconHouse style="margin-bottom: 3px" /></h5> Home</RouterLink>
+                    ><span class="d-flex"><BIconHouse class="me-2 mt-1" /> Home</span></RouterLink>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item ms-2">
                     <RouterLink
                         :class="['nav-link', { active: isActive('/playground') }]"
                         to="/playground"
-                    ><h5 class="d-inline"><BIconJoystick style="margin-bottom: 3px" /></h5> Playground</RouterLink>
+                    ><span class="d-flex"><BIconJoystick class="me-2 mt-1" /> Playground</span></RouterLink>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item ms-2">
                     <RouterLink
                         :class="['nav-link', { active: isActive('/docs') }]"
                         to="/docs"
-                    ><h5 class="d-inline"><BIconFileEarmarkText style="margin-bottom: 3px" /></h5> Documentations</RouterLink>
+                    ><span class="d-flex"><BIconFileEarmarkText class="me-2 mt-1" /> Documentations</span></RouterLink>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item ms-2">
                     <RouterLink
                         :class="['nav-link', { active: isActive('/references') }]"
                         to="/references"
-                    ><h5 class="d-inline"><BIconFileRichtext style="margin-bottom: 3px" /></h5>References</RouterLink>
+                    ><span class="d-flex"><BIconFileRichtext class="me-2 mt-1" /> References</span></RouterLink>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item ms-2">
                     <RouterLink
                         :class="['nav-link', { active: isActive('/download') }]"
                         to="/download"
-                    ><h5 class="d-inline"><BIconDownload style="margin-bottom: 3px" /></h5> Download</RouterLink>
+                    ><span class="d-flex"><BIconDownload class="me-2 mt-1" /> Download</span></RouterLink>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item ms-2">
                     <a
                         class="nav-link"
                         href="https://github.com/rhea-lang/rhea"
                         target="_blank"
-                    ><h5 class="d-inline"><BIconGithub style="margin-bottom: 3px" /></h5> GitHub</a>
+                    ><span class="d-flex"><BIconGithub class="me-2 mt-1" /> GitHub</span></a>
                 </li>
             </ul>
         </div>
