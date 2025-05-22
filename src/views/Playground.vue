bec455166cb8d2de7d799cbfd6acd3cd7eeb4a97
Nathanne Isip <nathanneisip@gmail.com>
2025-05-22 22:33:39 +0800

    Added navigational buttons on playground view.

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import Sandbox from "@/components/Sandbox.vue";
+import { BIconDownload, BIconFileEarmarkText, BIconFileRichtext } from "bootstrap-icons-vue";
 defineProps<{}>();
 </script>
 
 <template>
-    <div class="px-4">
-        <Sandbox height="50" />
+    <div class="container mt-4">
+        <div class="px-4">
+            <Sandbox height="45" />
+            <div class="btn-group header-btn-group w-100 mt-4 px-2" role="group">
+                <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/docs"><BIconFileEarmarkText class="me-2" /><span class="pl-2">Language</span></RouterLink>
+                <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/references"><BIconFileRichtext class="me-2" /><span class="pl-2">References</span></RouterLink>
+                <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/download"><BIconDownload class="me-2" /><span class="pl-2">Download</span></RouterLink>
+            </div>
+        </div>
     </div>
 </template>
