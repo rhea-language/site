2b193a57f5d3d85cca1120b561a2a38e80f2e797
Nathanne Isip <nathanneisip@gmail.com>
2025-05-23 07:19:36 +0800

    Included stylesheet on main script file.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "primeicons/primeicons.css";
 import "@/assets/base.css";
 import "@/assets/bootstrap.css";
 
