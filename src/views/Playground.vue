923bffb9ae769fd122434ed1b99bc40914cb965f
Nathanne Isip <nathanneisip@gmail.com>
2025-05-23 07:21:27 +0800

    Improve playground view layout

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -6,13 +6,12 @@ defineProps<{}>();
 
 <template>
     <div class="container mt-4">
-        <div class="px-4">
-            <Sandbox height="45" />
-            <div class="btn-group header-btn-group w-100 mt-4 px-2" role="group">
-                <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/docs"><BIconFileEarmarkText class="me-2" /><span class="pl-2">Language</span></RouterLink>
-                <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/references"><BIconFileRichtext class="me-2" /><span class="pl-2">References</span></RouterLink>
-                <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/download"><BIconDownload class="me-2" /><span class="pl-2">Download</span></RouterLink>
-            </div>
+        <h2 class="mb-4">Playground</h2>
+        <Sandbox height="35" />
+        <div class="btn-group header-btn-group w-100 mt-4 px-2" role="group">
+            <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/docs"><BIconFileEarmarkText class="me-2" /><span class="pl-2">Language</span></RouterLink>
+            <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/references"><BIconFileRichtext class="me-2" /><span class="pl-2">References</span></RouterLink>
+            <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/download"><BIconDownload class="me-2" /><span class="pl-2">Download</span></RouterLink>
         </div>
     </div>
 </template>
