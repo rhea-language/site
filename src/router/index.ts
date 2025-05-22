79fb36778fdb69ad0c9b5e5f28d82cd5f4bfed6b
Nathanne Isip <nathanneisip@gmail.com>
2025-05-23 07:22:57 +0800

    Added Download view layout on router for /download route.

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
 import Playground from "@/views/Playground.vue";
+import Download from "@/views/Download.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
             name: 'playground',
             component: Playground
         },
+        {
+            path: '/download',
+            name: 'download',
+            component: Download
+        }
     ],
 });
 
