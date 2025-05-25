dc96fe358e137d21966162f3d32d4e37dfe3c3c0
Nathanne Isip <nathanneisip@gmail.com>
2025-05-25 18:01:46 +0800

    Update icons on button below the sandbox in playground view layout.

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -9,9 +9,17 @@ defineProps<{}>();
         <h2 class="mb-4">Playground</h2>
         <Sandbox height="35" />
         <div class="btn-group header-btn-group w-100 mt-4 px-2" role="group">
-            <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/docs"><BIconFileEarmarkText class="me-2" /><span class="pl-2">Language</span></RouterLink>
-            <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/references"><BIconFileRichtext class="me-2" /><span class="pl-2">References</span></RouterLink>
-            <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/download"><BIconDownload class="me-2" /><span class="pl-2">Download</span></RouterLink>
+            <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/docs">
+                <BIconFileEarmarkText class="me-2" /><span class="pl-2">Language</span>
+            </RouterLink>
+
+            <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/references">
+                <BIconFileRichtext class="me-2" /><span class="pl-2">References</span>
+            </RouterLink>
+
+            <RouterLink class="btn btn-outline-primary w-100 bg-inherit" to="/download">
+                <BIconDownload class="me-2" /><span class="pl-2">Download</span>
+            </RouterLink>
         </div>
     </div>
 </template>
