c139c2927e62c32fc5a8f763e93815d73e7aa6e2
Nathanne Isip <nathanneisip@gmail.com>
2025-05-25 18:03:26 +0800

    Initial documentation table of contents as component.

---
+++ src/components/TableOfContents.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <div class="btn-group-vertical w-100">
+        <RouterLink
+            to="/docs"
+            class="btn btn-outline-primary text-start fw-bold"
+            active-class="active"
+            exact-active-class="active"
+        >1 &mdash; Overview</RouterLink>
+        <RouterLink
+            to="/docs/2-installation"
+            class="btn btn-outline-primary text-start fw-bold"
+            active-class="active"
+            exact-active-class="active"
+        >2 &mdash; Installation</RouterLink>
+        <RouterLink
+            to="/docs/3-literals"
+            class="btn btn-outline-primary text-start fw-bold"
+            active-class="active"
+            exact-active-class="active"
+        >3 &mdash; Literals</RouterLink>
+        <RouterLink
+            to="/docs/4-expressions"
+            class="btn btn-outline-primary text-start fw-bold"
+            active-class="active"
+            exact-active-class="active"
+        >4 &mdash; Expressions</RouterLink>
+        <RouterLink
+            to="/docs/5-func-mods"
+            class="btn btn-outline-primary text-start fw-bold"
+            active-class="active"
+            exact-active-class="active"
+        >5 &mdash; Functions and Modules</RouterLink>
+        <RouterLink
+            to="/docs/6-concurrency-error"
+            class="btn btn-outline-primary text-start fw-bold"
+            active-class="active"
+            exact-active-class="active"
+        >6 &mdash; Concurrency and Error Handling</RouterLink>
+        <RouterLink
+            to="/docs/7-comments"
+            class="btn btn-outline-primary text-start fw-bold"
+            active-class="active"
+            exact-active-class="active"
+        >7 &mdash; Comments</RouterLink>
+        <RouterLink
+            to="/docs/8-program-structure"
+            class="btn btn-outline-primary text-start fw-bold"
+            active-class="active"
+            exact-active-class="active"
+        >8 &mdash; Program Structure</RouterLink>
+    </div>
+</template>
