2631b5a46aad326e9e2cca5aa002925689635056
Nathanne Isip <nathanneisip@gmail.com>
2025-05-25 18:04:32 +0800

    Added documentation pages on router definitions.

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,24 +3,36 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import Playground from "@/views/Playground.vue";
 import Download from "@/views/Download.vue";
+import Documentations from "@/views/Documentations.vue";
+import Installation from "@/views/docs/Installation.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/',
-            name: 'home',
+            path: "/",
+            name: "home",
             component: HomeView,
         },
         {
-            path: '/playground',
-            name: 'playground',
+            path: "/playground",
+            name: "playground",
             component: Playground
         },
         {
-            path: '/download',
-            name: 'download',
+            path: "/download",
+            name: "download",
             component: Download
+        },
+        {
+            path: "/docs",
+            name: "docs",
+            component: Documentations
+        },
+        {
+            path: "/docs/2-installation",
+            name: "2-installation",
+            component: Installation
         }
     ],
 });
