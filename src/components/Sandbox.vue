acf5c09cc8a73137d25dda26ca095353f834f244
Nathanne Isip <nathanneisip@gmail.com>
2025-05-30 04:04:55 +0800

    Improved sandbox functionalities and implementations.

--- src/components/Sandbox.vue
+++ src/components/Sandbox.vue
@@ -3,231 +3,210 @@ import {
     BIconArrowClockwise,
     BIconPlay
 } from "bootstrap-icons-vue";
-</script>
 
-<script lang="ts">
 import "xterm/css/xterm.css";
 import * as monaco from "monaco-editor";
 
 import { Terminal } from "xterm";
 import { FitAddon } from "xterm-addon-fit";
+import {
+    ref,
+    watch,
+    onMounted,
+    onBeforeUnmount
+} from "vue";
+import { useWasmRuntime } from "@/composables/useWasmRuntime";
+
+const {
+    executeSource,
+    setActiveTerminalCallbacks,
+    clearActiveTerminalCallbacks
+} = useWasmRuntime();
+const props = withDefaults(
+    defineProps<{
+        value?: string;
+        title?: string;
+        height: string;
+        showExamples?: boolean;
+    }>(),
+    {showExamples: true}
+);
+const emit = defineEmits<{
+  (e: "input", val: string): void;
+}>();
 
 const languageId: string = "rhea";
-const examples: {[key: string]: string} = {
+const examples: Record<string, string> = {
     "hello-world": "#!/usr/bin/rhea\r\n\r\nval greet = func(name)\r\n\trender! \"Hello, \" + name + \"!\"\r\ngreet(\"world\")\r\n",
     "fibonacci": "#!/usr/bin/rhea\r\n\r\nfib = func(n) {\r\n\tret if(n <= 1) n\r\n\t\telse fib(n - 1) + fib(n - 2)\r\n}\r\n\r\nvalues = []\r\nloop(i = 0; i < 10; i = i + 1)\r\n\tvalues = values + fib(i)\r\n\r\nret values\r\n",
     "colors": "#!/usr/bin/rhea\r\n\r\nenum Color {\r\n\tRed   = 1,\r\n\tGreen = 2,\r\n\tBlue  = 3\r\n}\r\n\r\nprint_color = func(color)\r\n\twhen(color) {\r\n\t\tif(Color.Red)   \"red\",\r\n\t\tif(Color.Green) \"green\",\r\n\t\telse            \"blue\"\r\n\t}\r\n\r\nrender \"The color is \" + print_color(Color.Green)\r\n",
     "99-beers": "#!/usr/bin/rhea\r\n\r\nval count = 99\r\nwhile(count > 0) {\r\n\tval bottle = if(count == 1) \"bottle\" else \"bottles\"\r\n\r\n\trender! count + \" \" + bottle + \" of beer on the wall\"\r\n\trender! count + \" \" + bottle + \" of beer,\"\r\n\trender! \"Take one down, pass it around,\"\r\n\r\n\tcount = count - 1\r\n\tif(count > 0)\r\n\t\trender! count + \" \" + bottle + \" of the beer on the wall.\\r\\n\"\r\n\telse render! \"\\r\\nNo more \" + bottle + \" of beer on the wall.\"\r\n}\r\n"
 };
 
+const editorContainer = ref<HTMLElement>();
+const terminalContainer = ref<HTMLElement>();
 let editor: monaco.editor.IStandaloneCodeEditor,
-    terminal: Terminal,
-    executeSource: Function = (_: string)=> {};
+    terminal: Terminal;
 
-declare global {
-    interface Window {
-        Module: any;
-        FS: any;
+const runId = "run-btn-" + Math.random()
+    .toString()
+    .substring(2, 14);
+
+watch(
+    ()=> props.value,
+    (newVal)=> {
+        if(editor && editor.getValue() !== newVal)
+            editor.setValue(newVal as string);
     }
+);
+
+function isDarkMode() {
+    return document.body.getAttribute("data-bs-theme") == "dark";
 }
 
-export default {
-    name: "code-editor",
-    props: {
-        value: {
-            type: String,
-            required: true,
-            default: ""
-        },
-        title: {
-            type: String,
-            required: false,
-            default: ""
-        },
-        height: {
-            type: String,
-            required: true,
-            default: 0
-        },
-        showExamples: {
-            type: Boolean,
-            required: false,
-            default: true
-        }
-    },
-    data() {
-        return {
-            Module: null,
-            FS: null
-        }
-    },
-    watch: {
-        value(newVal) {
-            if(editor && editor.getValue() !== newVal)
-                editor.setValue(newVal);
-        }
-    },
-    methods: {
-        isDarkMode() {
-            return document.body.getAttribute("data-bs-theme") == "dark";
-        },
-        exampleSelected(event: Event) {
-            editor.setValue(examples[(event.target as HTMLSelectElement).value]);
-        },
-        loadWasmModule() {
-            const script = document.createElement('script');
-            script.src = '/rhea.js';
+function exampleSelected(event: Event) {
+    editor.setValue(examples[(event.target as HTMLSelectElement).value]);
+}
 
-            script.onerror = () => {
-                terminal.clear();
-                terminal.writeln('Failed to Rhea WebAssembly interpreter');
-            };
-            script.onload = () => {
-                if(typeof window.Module === 'undefined')
-                    return;
+function resetCode() {
+    if(props.value)
+        editor.setValue(props.value);
+}
 
-                (window.FS as any).init(
-                    () => null,
-                    (c: number) => terminal.write(String.fromCharCode(c)),
-                    (c: number) => terminal.write(String.fromCharCode(c))
-                );
-                (window.Module as any).onRuntimeInitialized = () => {
-                    executeSource = (source: string)=>
-                        window.Module.ccall(
-                            "executeSource",
-                            null,
-                            ["string"],
-                            [source]
-                        );
-                };
-            };
+function applyTheme() {
+    const theme = localStorage.getItem("color") || "dark";
+    const isDark = theme === "dark";
 
-            document.body.appendChild(script);
-        },
-        resetCode() {
-            if(this.value)
-                editor.setValue(this.value);
-        },
-        runCode() {
-            terminal.clear();
-            executeSource(editor.getValue());
-            terminal.writeln("\r\n\u001b[1;34mExecution completed.\u001b[0m");
-        },
-        applyTheme() {
-            const theme = localStorage.getItem("color") || "dark";
-            const isDark = theme === "dark";
+    monaco.editor.setTheme("rhea-theme-" + theme);
+    if(terminal)
+        terminal.options.theme = {
+            background: isDark ? "#000000" : "#ffffff",
+            foreground: isDark ? "#ffffff" : "#000000"
+        };
 
-            monaco.editor.setTheme("rhea-theme-" + theme);
-            terminal.options.theme = {
-                background: isDark ? "#000000" : "#ffffff",
-                foreground: isDark ? "#ffffff" : "#000000"
-            };
+    return theme;
+}
 
-            return theme;
-        },
-        handleStorageChange(event: StorageEvent) {
-            if(event.key === "color")
-                document.body.setAttribute(
-                    "data-bs-theme",
-                    this.applyTheme()
-                );
-        }
-    },
-    mounted() {
-        this.loadWasmModule();
-        monaco.languages.register({id: languageId});
-        monaco.editor.defineTheme("rhea-theme-dark", {
-            base: "vs-dark" as monaco.editor.BuiltinTheme,
-            inherit: true,
-            rules: [],
-            colors: {"editor.background": "#000000"}
-        });
-        monaco.editor.defineTheme("rhea-theme-light", {
-            base: "vs" as monaco.editor.BuiltinTheme,
-            inherit: true,
-            rules: [],
-            colors: {"editor.background": "#ffffff"}
-        });
+function handleStorageChange(event: StorageEvent) {
+    if(event.key === "color")
+        document.body.setAttribute(
+            "data-bs-theme",
+            applyTheme()
+        );
+}
 
-        monaco.languages.setMonarchTokensProvider(languageId, {
-            tokenizer: {
-                root: [
-                    [/(break|catch|continue|delete|else|enum|false|from|func|halt|handle|import|if|lock|loop|maybe|mod|nil|parallel|random|render|ret|size|test|then|throw|true|type|unless|use|val|wait|when|while)\b/, "keyword.control.rhea"],
-                    [/(0b[01]+|0t[0-2]+|0c[0-7]+|0x[0-9a-fA-F]+|\d+(\.\d*)?)/, "digit.numberic.rhea"],
-                    [/#[^\n]*/, "comment.line.rhea"],
-                    [/"(?:\\.|[^"\\])*"/, "string.quoted.double.rhea"],
-                ],
-            },
-        });
+onMounted(()=> {
+    monaco.languages.register({id: languageId});
+    monaco.editor.defineTheme("rhea-theme-dark", {
+        base: "vs-dark" as monaco.editor.BuiltinTheme,
+        inherit: true,
+        rules: [],
+        colors: {"editor.background": "#000000"}
+    });
+    monaco.editor.defineTheme("rhea-theme-light", {
+        base: "vs" as monaco.editor.BuiltinTheme,
+        inherit: true,
+        rules: [],
+        colors: {"editor.background": "#ffffff"}
+    });
 
-        monaco.languages.setLanguageConfiguration(languageId, {
-            comments: {lineComment: "#"},
-            brackets: [
-                ["{", "}"],
-                ["[", "]"],
-                ["(", ")"]
+    monaco.languages.setMonarchTokensProvider(languageId, {
+        tokenizer: {
+            root: [
+                [/(break|catch|continue|delete|else|enum|false|from|func|halt|handle|import|if|lock|loop|maybe|mod|nil|parallel|random|render|ret|size|test|then|throw|true|type|unless|use|val|wait|when|while)\b/, "keyword.control.rhea"],
+                [/(0b[01]+|0t[0-2]+|0c[0-7]+|0x[0-9a-fA-F]+|\d+(\.\d*)?)/, "digit.numberic.rhea"],
+                [/#[^\n]*/, "comment.line.rhea"],
+                [/"(?:\\.|[^"\\])*"/, "string.quoted.double.rhea"],
             ],
-            autoClosingPairs: [
-                { open: "{", close: "}" },
-                { open: "[", close: "]" },
-                { open: "(", close: ")" },
-                { open: '"', close: '"' }
-            ]
-        });
+        },
+    });
 
-        editor = monaco.editor.create((this.$refs.editorContainer as HTMLElement), {
-            value: this.value,
-            language: languageId,
-            theme: "rhea-theme",
-            minimap: {enabled: false},
-            automaticLayout: true
-        });
-  
-        editor.onDidChangeModelContent(() => {
-            const newValue = editor.getValue();
-            if(newValue !== this.value)
-                this.$emit("input", newValue);
-        });
+    monaco.languages.setLanguageConfiguration(languageId, {
+        comments: {lineComment: "#"},
+        brackets: [
+            ["{", "}"],
+            ["[", "]"],
+            ["(", ")"]
+        ],
+        autoClosingPairs: [
+            { open: "{", close: "}" },
+            { open: "[", close: "]" },
+            { open: "(", close: ")" },
+            { open: '"', close: '"' }
+        ]
+    });
 
-        if(!this.value || this.value == "")
-            editor.setValue(examples["99-beers"]);
-        else editor.setValue(this.value);
+    editor = monaco.editor.create(editorContainer.value!, {
+        value: props.value,
+        language: languageId,
+        theme: "rhea-theme",
+        minimap: {enabled: false},
+        automaticLayout: true
+    });
 
-        terminal = new Terminal({
-            rows: (!this.value || this.value == "") ? 7 : 5,
-            cols: Math.floor((this.$refs.terminalContainer as HTMLElement).offsetWidth / 9) - 2,
-            cursorBlink: true,
-            theme: {
-                background: this.isDarkMode() ? "#000000" : "#ffffff",
-                foreground: this.isDarkMode() ? "#ffffff" : "#000000"
+    editor.onDidChangeModelContent(() => {
+        const newValue = editor.getValue();
+        if(newValue !== props.value)
+            emit("input", newValue);
+    });
+
+    if(!props.value || props.value == "")
+        editor.setValue(examples["99-beers"]);
+    else editor.setValue(props.value);
+
+    terminal = new Terminal({
+        rows: (!props.value || props.value == "") ? 7 : 5,
+        cols: Math.floor(terminalContainer.value!.offsetWidth / 9) - 2,
+        cursorBlink: true,
+        theme: {
+            background: isDarkMode() ? "#000000" : "#ffffff",
+            foreground: isDarkMode() ? "#ffffff" : "#000000"
+        }
+    });
+
+    terminal.loadAddon(new FitAddon());
+    terminal.open(terminalContainer.value!);
+    terminal.writeln("\x1b[94mStatus\x1b[0m: Ready");
+
+    applyTheme();
+    window.addEventListener(
+        "storage",
+        handleStorageChange
+    );
+
+    const observer = new MutationObserver(() => {
+        applyTheme();
+    });
+    observer.observe(
+        document.body,
+        {
+            attributes: true,
+            attributeFilter: ['data-bs-theme']
+        }
+    );
+
+    const runBtn = document.getElementById(runId);
+    if(runBtn)
+        runBtn.onclick = ()=> {
+            terminal.clear();
+            setActiveTerminalCallbacks(
+                (c: number) => terminal.write(String.fromCharCode(c)),
+                (c: number) => terminal.write(String.fromCharCode(c))
+            );
+
+            if(executeSource.value) {
+                executeSource.value(editor.getValue());
+                terminal.writeln("\r\n\u001b[1;34mExecution completed.\u001b[0m");
             }
-        });
+            else terminal.writeln(
+                "\u001b[1;31mError: Rhea interpreter not loaded or initialized.\u001b[0m"
+            );
+        };
 
-        terminal.loadAddon(new FitAddon());
-        terminal.open(this.$refs.terminalContainer as HTMLElement);
-        terminal.writeln("\x1b[94mStatus\x1b[0m: Ready");
-
-        this.applyTheme();
-        window.addEventListener(
-            "storage",
-            this.handleStorageChange
-        );
-
-        const observer = new MutationObserver(() => {
-            this.applyTheme();
-        });
-        observer.observe(
-            document.body,
-            {
-                attributes: true,
-                attributeFilter: ['data-bs-theme']
-            }
-        );
-    },
-    beforeDestroy() {
+    onBeforeUnmount(()=> {
         window.removeEventListener(
             "storage",
-            this.handleStorageChange
+            handleStorageChange
         );
 
         if(editor)
@@ -235,12 +214,14 @@ export default {
 
         if(terminal)
             terminal.dispose();
-    }
-};
+
+        clearActiveTerminalCallbacks();
+    });
+});
 </script>
 
 <template>
-    <div v-if="showExamples" class="row mb-2">
+    <div v-if="props.showExamples" class="row mb-2">
         <div class="col-8">
             <select class="form-control form-select-sm w-100" @change="exampleSelected($event)">
                 <option value="99-beers">99 Beers</option>
@@ -251,19 +232,19 @@ export default {
         </div>
 
         <div class="col-4">
-            <button class="btn btn-primary w-100 py-1" @click="runCode">
+            <button class="btn btn-primary w-100 py-1" :id="runId">
                 <h5 class="d-inline"><BIconPlay /></h5> <span class="desktop-only">Run</span>
             </button>
         </div>
     </div>
 
-    <div v-if="!showExamples" class="card bg-transparent shadow-lg border border-primary mt-2 mb-4 mx-lg-4">
+    <div v-if="!props.showExamples" class="card bg-transparent shadow-lg border border-primary mt-2 mb-4 mx-lg-4">
         <div class="card-header">
             <div class="row w-100">
                 <div class="col-lg-8 col-7 pt-1 text-white">{{ title }}</div>
                 <div class="col-lg-4 col-5 m-0 p-0" align="right">
                     <div class="btn-group w-100 m-0 p-0" role="button">
-                        <button class="btn btn-outline-primary bg-white text-primary w-100 py-1" @click="runCode">
+                        <button class="btn btn-outline-primary bg-white text-primary w-100 py-1" :id="runId">
                             <h5 class="d-inline"><BIconPlay /></h5> <span class="desktop-only">Run</span>
                         </button>
                         <button class="btn btn-outline-primary bg-white text-primary w-100 py-1" @click="resetCode">
@@ -293,7 +274,7 @@ export default {
             :style="{ height: `${height}vh` }"
         ></div>
 
-        <div v-if="showExamples" class="mt-3"></div>
+        <div v-if="props.showExamples" class="mt-3"></div>
         <div 
             class="terminal-container border"
             ref="terminalContainer"
